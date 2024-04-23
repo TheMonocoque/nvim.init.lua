12045d397ebcd131e4217ddaaa8874f1b5c398ff
TheMonocoque <7273463+TheMonocoque@users.noreply.github.com>
2024-04-23 07:19:41 -0400

    add lsp definition

--- lua/veritas/init.lua
+++ lua/veritas/init.lua
@@ -1,3 +1,13 @@
 require("veritas.set")
 require("veritas.remap")
 require("veritas.lazy_init")
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('LspAttach', {
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+    end
+})
