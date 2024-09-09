d9ee8aee4c6da90d5601b8c32a71d829949aa592
TheMonocoque <7273463+TheMonocoque@users.noreply.github.com>
2024-09-09 18:44:15 -0400

    Create gitworktree.lua

---
+++ lua/veritas/lazy/gitworktree.lua
@@ -0,0 +1,8 @@
+return {
+    "ThePrimeagen/git-worktree.nvim",
+
+    config = function()
+        vim.keymap.set("n", "<leader>sr", "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>")
+        vim.keymap.set("n", "<leader>sR", "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree(<CR>")
+    end
+}
