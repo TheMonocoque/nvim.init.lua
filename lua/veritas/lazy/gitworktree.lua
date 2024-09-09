16f00c68bd6b8b2307799156a914e03ead5bb986
TheMonocoque <7273463+TheMonocoque@users.noreply.github.com>
2024-09-09 18:47:04 -0400

    Update gitworktree.lua

--- lua/veritas/lazy/gitworktree.lua
+++ lua/veritas/lazy/gitworktree.lua
@@ -3,6 +3,6 @@ return {
 
     config = function()
         vim.keymap.set("n", "<leader>sr", "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>")
-        vim.keymap.set("n", "<leader>sR", "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree(<CR>")
+        vim.keymap.set("n", "<leader>sR", "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>")
     end
 }
