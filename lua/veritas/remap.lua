6076deaf5c1fe60728dcefd33fb4e5e3ae661c20
TheMonocoque <7273463+TheMonocoque@users.noreply.github.com>
2024-09-23 17:57:06 -0400

    Update remap.lua with format tools

--- lua/veritas/remap.lua
+++ lua/veritas/remap.lua
@@ -1,8 +1,14 @@
-
+-- remap leader and explorer
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- movement
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- run this file, typically with /usr/bin/env
 vim.keymap.set("n", "<F9>", ':!%:p<CR>')
 
+-- callout to formatting tools
+vim.keymap.set("n", "<leader>fmb", "<CMD>!black %<CR>")
+vim.keymap.set("n", "<leader>fmj", "<CMD>!jsonnetfmt -i -n 2 %<CR>")
